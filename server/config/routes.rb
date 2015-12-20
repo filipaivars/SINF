7d61b4c1698f69da02947539cc32c29e35bd7712
filipaivars <ivars.filipa@gmail.com>
2015-12-20 14:59:38 +0000

    coisas

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -8,8 +9,10 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
 
-  
-    post '/users' => 'users#create'
+
+
+    get '/singup' => 'users#create'
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
